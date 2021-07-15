dea51126155dac5ec51be4529090c85635c9acef
Payman <crygie@hotmail.com>
2021-07-15 14:20:25 -0700

    Added base files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/vendors/normalize.css'
+import '../styles/globals/base.scss'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
